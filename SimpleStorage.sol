2ee6fa545dc99012e3219dd807a7e7205c4fe479
Adam Ramski <adam.ramski@gmail.com>
2026-05-19 19:08:09 +0200

    add an array of persons (struct) and addPerson function

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,14 +3,26 @@ pragma solidity 0.8.19;
 
 contract SimpleStorage {
     uint256 myFavoriteNumber;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    Person[] public listOfPeople;
 
     function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
 
-
     // view - read state from the blockchain
-    function retrieve() public view returns(uint256) {
+    function retrieve() public view returns (uint256) {
         return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        Person memory newPerson = Person({
+            name: _name,
+            favoriteNumber: _favoriteNumber
+        });
+        listOfPeople.push(newPerson);
     }
 }
