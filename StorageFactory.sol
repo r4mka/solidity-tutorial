f1580eb7dc0059ba27c455e1e7dd5bff881fbe49
Adam Ramski <adam.ramski@gmail.com>
2026-05-20 00:47:25 +0200

    add bracket spacing to prettier config

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import {SimpleStorage} from "./SimpleStorage.sol";
+import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory {
     SimpleStorage[] public listOfSimpleStorageContracts;
