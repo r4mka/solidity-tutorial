c8d5c21382f3415f0430bc754e00a764d2897923
Adam Ramski <adam.ramski@gmail.com>
2026-05-20 00:42:43 +0200

    add sfStore and sfGet to StorageFactory

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,13 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import { SimpleStorage } from "./SimpleStorage.sol";
+import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract StorageFactory {
-    SimpleStorage[] public storages;
+    SimpleStorage[] public listOfSimpleStorageContracts;
 
     function createSimpleStorageContract() public {
         SimpleStorage newStorage = new SimpleStorage();
-        storages.push(newStorage);
+        listOfSimpleStorageContracts.push(newStorage);
+    }
+
+    function sfStore(uint256 _index, uint256 _number) public {
+        SimpleStorage simpleStorage = listOfSimpleStorageContracts[_index];
+        simpleStorage.store(_number);
+    }
+
+    function sfGet(uint256 _index) public view returns (uint256) {
+        SimpleStorage simpleStorage = listOfSimpleStorageContracts[_index];
+        return simpleStorage.retrieve();
     }
 }
