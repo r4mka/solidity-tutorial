23baf221e43b097058dfda79605325a67f734d16
Adam Ramski <adam.ramski@gmail.com>
2026-05-19 18:38:03 +0200

    add retrieve view function to SimpleStorage contract

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,9 +2,15 @@
 pragma solidity 0.8.19;
 
 contract SimpleStorage {
-    uint256 public myFavoriteNumber;
+    uint256 myFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
+    }
+
+
+    // view - read state from the blockchain
+    function retrieve() public view returns(uint256) {
+        return myFavoriteNumber;
     }
 }
