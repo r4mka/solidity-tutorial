5f0a70d9781980404c416261ea96844977f14a43
Adam Ramski <adam.ramski@gmail.com>
2026-05-20 00:55:53 +0200

    add new contract that inherits from SimpleStorage and overrides its method

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -10,7 +10,7 @@ contract SimpleStorage {
     Person[] public listOfPeople;
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public virtual {
         myFavoriteNumber = _favoriteNumber;
     }
 
