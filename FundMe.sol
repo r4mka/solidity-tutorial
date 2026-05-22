bb7e3e9d6b8a8def38737c7c6f6caab1b6e5950e
Adam Ramski <adam.ramski@gmail.com>
2026-05-22 11:34:56 +0200

    introduce constant and immutable variables

--- FundMe.sol
+++ FundMe.sol
@@ -3,21 +3,31 @@ pragma solidity ^0.8.18;
 
 import { PriceConverter } from "./PriceConverter.sol";
 
+/* 
+    transaction costs:
+    772,281 - initial 
+    752,546 - make minUSD constant
+*/
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minUSD = 5e18; // we want all numbers to have 18 decimals
+    uint256 public constant MIN_USD = 5e18; // we want all numbers to have 18 decimals
+    // 303   - constant
+    // 2,402 - non-constant
 
-    address public owner;
+    address public immutable i_owner;
+    // 439   - immutable
+    // 2,551 - non-immutable 
+
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= minUSD, "minimum contribution is 5 USD");
+        require(msg.value.getConversionRate() >= MIN_USD, "minimum contribution is 5 USD");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
@@ -34,7 +44,7 @@ contract FundMe {
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Sender is not owner!");
+        require(msg.sender == i_owner, "Sender is not owner!");
         _;
     }
 }
